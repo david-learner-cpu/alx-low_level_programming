3a5e248337c8b8fae39ce1be20006b8a7040e18c
david-learner-cpu <davidgaruba23@gmail.com>
2022-09-12 03:41:04 -0700

    4p

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,13 @@
-echo '#include<stdio.h>
+#!/bin/bash
+#include<stdio.h>
 
 /**
  * main - Entry point
- * Description: prints "\"Programming is like building a multilingual puzzle"*
+ *
  * Return: Always 0 (Success)
- * /
-
+ */
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
-}' > 4-puts.c
+	return(0);
+}
