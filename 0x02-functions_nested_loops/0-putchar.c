3967109e15ec0ab8b0312549e2fe932de4fa4983
david-learner-cpu <davidgaruba23@gmail.com>
2022-09-14 06:12:21 -0700

    0char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <main.h>
+
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char *sh = "Dave";
+
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
