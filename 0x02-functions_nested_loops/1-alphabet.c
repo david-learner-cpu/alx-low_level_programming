4369494f768fc5fa2b73f34479fc631277b8ddfe
david-learner-cpu <davidgaruba23@gmail.com>
2022-09-14 07:41:52 -0700

    1a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * main - Prints the alphabets in lowercase and followed by a new line
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	int n;
+	for (n=a; n <= A; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
