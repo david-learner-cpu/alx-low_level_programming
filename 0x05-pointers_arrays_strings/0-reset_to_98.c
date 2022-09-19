f52ffba7374c0cd515ea2c5a44359c2d907a1eeb
david-learner-cpu <davidgaruba23@gmail.com>
2022-09-19 03:51:23 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * reset_to_98 - function that takes a pointer to an int as parameter and
- * updates the value of it points to 98
+ * reset_to_98 - function that takes a pointer to an int as parameter and updates the value of its point to 98
  *
  * @n: This is the entry
+ *
  */
 
 void reset_to_98(int *n)
