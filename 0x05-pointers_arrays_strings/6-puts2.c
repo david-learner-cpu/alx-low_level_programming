095883331e01cde890c2dedeb7eb38b70f2a14c9
david-learner-cpu <davidgaruba23@gmail.com>
2022-09-21 05:06:05 -0700

    6. Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,10 +12,10 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (*(str + i) != '\0)
+	while (*(str + i) != '\0')
 	{
 		if (i % 2 == 0)
-			putchar(*(str +i));
+			putchar(*(str + i));
 		i++;
 	}
 	putchar(10);
