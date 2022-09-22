10b790da12ea563e8c1ecf6fce8f1f034934c58d
David Oshiolene Garuba <71131005+david-learner-cpu@users.noreply.github.com>
2022-09-22 12:09:09 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,18 +2,15 @@
 
 /**
  * _strcat - concatenates the string pointed to by @src to
- *
  * the end of the string pointed to by @dest
- *
  * @dest: String that will be appended
- *
  * @src: String to be concatenated upon
  *
  * Return: returns pointer to @dest
  */
-
 char *_strcat(char *dest, char *src)
 {
+
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
