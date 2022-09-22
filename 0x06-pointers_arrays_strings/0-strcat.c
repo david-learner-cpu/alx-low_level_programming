fae3060763023ce2c3e44b8bd0566449f81f7818
David Oshiolene Garuba <71131005+david-learner-cpu@users.noreply.github.com>
2022-09-22 14:45:09 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,10 +14,10 @@ char *_strcat(char *dest, char *src)
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
-		dest_len++;
+	dest_len++;
 
 	for (index = 0; src[index]; index++)
-		[dest_len++] = src[index];
+	[dest_len++] = src[index];
 
 	return (dest);
 }
